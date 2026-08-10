15747c3f55d19c305f1b6cfa1a35340e1cd64c70
matheussantos4 <matheus.santos88y@gmail.com>
2026-08-10 19:41:15 -0300

    feat: adicionar, remover e listar tarefas pré estabelecido

--- GerenciadorDeTarefas/Sistema/GerenciadorTarefas.java
+++ GerenciadorDeTarefas/Sistema/GerenciadorTarefas.java
@@ -3,22 +3,22 @@ package Sistema;
 import java.util.ArrayList;
 
 public class GerenciadorTarefas {
+
     ArrayList<Tarefa> tarefas = new ArrayList<>();
 
     public void adicionarTarefa(Tarefa tarefa) {
         tarefas.add(tarefa);
     }
 
+    public void removerTarefa(Tarefa tarefa) {
+        tarefas.remove(tarefa);
+    }
+
     public void listarTarefas() {
-        for (int i = 0; i < tarefas.size(); i++) {
-            if (tarefas.isEmpty()) {
-                System.out.println("Não há tarefas.");
-            } else {
-            System.out.println(tarefas.get(i).getNome());}
-        }
+        for (Tarefa tarefa: tarefas) {
+            System.out.println(tarefa.getNome());}
     }
 
     public void removerTarefa() {
-
     }
 }
