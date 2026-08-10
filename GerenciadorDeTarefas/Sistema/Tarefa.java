b5d0dcfbe9ae97eb0590c3dc8c40867361842b2c
matheussantos4 <matheus.santos88y@gmail.com>
2026-08-10 20:06:09 -0300

    feat: desenvolvimento classe tarefa.

--- GerenciadorDeTarefas/Sistema/Tarefa.java
+++ GerenciadorDeTarefas/Sistema/Tarefa.java
@@ -2,26 +2,40 @@ package Sistema;
 
 public class Tarefa {
     private String nome;
-    private double data;
+    private int ID;
+    private Estagios status;
 
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
+        if (nome != null && !nome.isBlank()) {
+            this.nome = nome;
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        if (ID > 0) {
+            this.ID = ID;
+        }
+    }
+
+    public Estagios getStatus() {
+        return status;
+    }
+
+    public void setStatus(Estagios status) {
+        this.status = status;
+    }
+
+    public Tarefa(String nome, int ID) {
         this.nome = nome;
-    }
-
-    public double getData() {
-        return data;
-    }
-
-    public void setData(double data) {
-        this.data = data;
-    }
-
-    public Tarefa(String nome, double data) {
-        this.nome = nome;
-        this.data = data;
+        this.ID = ID;
+        this.status = Estagios.PENDENTE;
     }
 }
