0fe5b1c8f76f2555d97725b86710e52ab9c3342c
matheussantos4 <matheus.santos88y@gmail.com>
2026-08-10 20:54:18 -0300

    doc: descrição de funcionalidades.

--- GerenciadorDeTarefas/Sistema/Tarefa.java
+++ GerenciadorDeTarefas/Sistema/Tarefa.java
@@ -9,13 +9,15 @@ public class Tarefa {
     public String getNome() {
         return nome;
     }
-
+    
+    // Set nome para garantir que não possa ser preenchido vazio.
     public void setNome(String nome) {
         if (nome != null && !nome.isBlank()) {
             this.nome = nome;
         }
     }
 
+    // Status tarefa vindo do enum Estagios.
     public Estagios getStatus() {
         return status;
     }
@@ -28,6 +30,7 @@ public class Tarefa {
         return ID;
     }
 
+    //Construtor da tarefa com Contador de id automático.
     public Tarefa(String nome) {
         contadorID++;
         this.nome = nome;
