afb07483c8261aef135c25b9d2513c176feed4bf
matheussantos4 <matheus.santos88y@gmail.com>
2026-08-10 19:24:09 -0300

    chore: criação GerenciadorTarefas.

--- GerenciadorDeTarefas/Sistema/GerenciadorTarefas.java
+++ GerenciadorDeTarefas/Sistema/GerenciadorTarefas.java
@@ -1,4 +1,24 @@
 package Sistema;
 
+import java.util.ArrayList;
+
 public class GerenciadorTarefas {
+    ArrayList<Tarefa> tarefas = new ArrayList<>();
+
+    public void adicionarTarefa(Tarefa tarefa) {
+        tarefas.add(tarefa);
+    }
+
+    public void listarTarefas() {
+        for (int i = 0; i < tarefas.size(); i++) {
+            if (tarefas.isEmpty()) {
+                System.out.println("Não há tarefas.");
+            } else {
+            System.out.println(tarefas.get(i).getNome());}
+        }
+    }
+
+    public void removerTarefa() {
+
+    }
 }
